645d4b435016f688b33c8a89e026cccf4b53ba3d
Tyler Nijmeh <tylernij@gmail.com>
2022-10-07 21:51:10 -0700

    Fix scraper for new examine layout
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 import re
 from markdownify import markdownify
@@ -8,14 +9,24 @@ excluded_classes = ['optin-wrapper', 'expand-items', 'th-module-head']
 
 base_url = 'https://examine.com'
 full_list = f'{base_url}/supplements'
-full_list_html = requests.get(full_list)
-full_list_soup = BeautifulSoup(full_list_html.content, "html.parser")
-all_supps = full_list_soup.find(class_='spi-2-all-supplements')
+
+session = HTMLSession()
+full_list_html = session.get(full_list)
+full_list_html.html.render(wait=2, sleep=2)
+full_list_soup = BeautifulSoup(full_list_html.html.html, 'html.parser')
 
 supplements = {}
-for supp_a in all_supps.findAll('a', href=True):
+all_supps = full_list_soup.find(id='all-supplements')
+all_supplement_links = all_supps.findAll('a', href=True)
+print(f'Count: {len(all_supplement_links)}')
+for supp_a in all_supplement_links:
     href = supp_a['href']
-    name = re.match(r'/supplements/(.*)/', href).group(1)
+    href_match = re.match(r'/supplements/(.*)/', href)
+    
+    if href_match is None:
+    	continue
+    
+    name = href_match.group(1)
     url = f'{base_url}{href}'
 
     if name in supplements:
